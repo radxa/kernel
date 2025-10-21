5a2eae43fe88d11622e26ad04b3f13a17d437109
ZHANG Yuntian <yt@radxa.com>
2025-10-21 11:34:35 +0000

    platform/chrome: fix build failure when CONFIG_CIX_EC=m
    #ifdef will return false when CONFIG_CIX_EC=m.
    Switch to IS_ENABLED() to handle this case.
    
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>
    Change-Id: If954959c94e5d1cf16ad7d7bb5543152bbfdad8c

--- drivers/platform/chrome/cros_ec_i2c.c
+++ drivers/platform/chrome/cros_ec_i2c.c
@@ -340,7 +340,7 @@ static int cros_ec_i2c_resume(struct device *dev)
 #endif
 
 static const struct dev_pm_ops cros_ec_i2c_pm_ops = {
-#ifdef CONFIG_CIX_EC
+#if IS_ENABLED(CONFIG_CIX_EC)
 	SYSTEM_SLEEP_PM_OPS(cros_ec_i2c_suspend, cros_ec_i2c_resume)
 #else
 	SET_LATE_SYSTEM_SLEEP_PM_OPS(cros_ec_i2c_suspend, cros_ec_i2c_resume)
