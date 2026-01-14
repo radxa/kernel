d69eb6b5df9386efab0cb2179993c99f67a190e1
Junhao Xie <bigfoot@radxa.com>
2026-01-14 10:48:52 +0800

    WIP: drm/msm/dp: Use compatible table to filter out certain modes to avoid instability
    Signed-off-by: Junhao Xie <bigfoot@radxa.com>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -912,6 +912,48 @@ static int msm_dp_display_disable(struct msm_dp_display_private *dp)
 	return 0;
 }
 
+static const struct msm_dp_bridge_mode_filter {
+	const char *compatible;
+	u32 max_pixels;
+	u32 max_vrefresh;
+} msm_dp_bridge_mode_filters[] = {
+	{ "radxa,dragon-q6a", 3440 * 1440, 30 },
+	{}
+};
+
+static bool msm_dp_bridge_mode_filter_valid(const struct drm_display_mode *mode)
+{
+	const struct msm_dp_bridge_mode_filter *filter;
+	struct device_node *np;
+	int vrefresh;
+	u32 pixels;
+
+	np = of_find_node_by_path("/");
+	if (!np)
+		return true;
+
+	pixels = mode->hdisplay * mode->vdisplay;
+	vrefresh = drm_mode_vrefresh(mode);
+
+	for (filter = msm_dp_bridge_mode_filters; filter->compatible; filter++) {
+		if (!of_device_is_compatible(np, filter->compatible))
+			continue;
+
+		if (pixels <= filter->max_pixels ||
+		    vrefresh <= filter->max_vrefresh)
+			continue;
+
+		DRM_INFO("mode filtered by %s: %ux%u@%u\n",
+			 filter->compatible, mode->hdisplay,
+			 mode->vdisplay, vrefresh);
+		of_node_put(np);
+		return false;
+	}
+
+	of_node_put(np);
+	return true;
+}
+
 /**
  * msm_dp_bridge_mode_valid - callback to determine if specified mode is valid
  * @bridge: Pointer to drm bridge structure
@@ -929,7 +971,6 @@ enum drm_mode_status msm_dp_bridge_mode_valid(struct drm_bridge *bridge,
 	u32 mode_rate_khz = 0, supported_rate_khz = 0, mode_bpp = 0;
 	struct msm_dp *dp;
 	int mode_pclk_khz = mode->clock;
-	int vrefresh = drm_mode_vrefresh(mode);
 
 	dp = to_dp_bridge(bridge)->msm_dp_display;
 
@@ -962,7 +1003,7 @@ enum drm_mode_status msm_dp_bridge_mode_valid(struct drm_bridge *bridge,
 	if (mode_rate_khz > supported_rate_khz)
 		return MODE_BAD;
 
-	if ((mode->hdisplay * mode->vdisplay > 3440 * 1440) && vrefresh > 30)
+	if (!msm_dp_bridge_mode_filter_valid(mode))
 		return MODE_BAD;
 
 	return MODE_OK;
