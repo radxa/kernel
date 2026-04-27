3d47b858c2400824b6822459a4b3b08684f4f2d3
Jiali Chen <chenjiali@radxa.com>
2026-04-27 18:10:44 +0800

    firmware: qcom: scm: Allow QSEECOM for Radxa CM-Q64
    Add "radxa,cm-q64" as compatible device for QSEECOM
    
    This is required to get access to efivars and uefi boot loader support.
    
    Signed-off-by: Jiali Chen <chenjiali@radxa.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -2339,6 +2339,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "qcom,x1e80100-crd" },
 	{ .compatible = "qcom,x1e80100-qcp" },
 	{ .compatible = "qcom,x1p42100-crd" },
+	{ .compatible = "radxa,cm-q64" },
 	{ .compatible = "radxa,dragon-q6a" },
 	{ .compatible = "radxa,dragon-q8b" },
 	{ }
@@ -2605,6 +2606,7 @@ EXPORT_SYMBOL_GPL(qcom_scm_storage_send_cmd);
  */
 static const struct of_device_id qcom_scm_storage_allowlist[] = {
 	{ .compatible = "radxa,dragon-q6a" },
+	{ .compatible = "radxa,cm-q64" },
 	{ .compatible = "radxa,dragon-q8b" },
 	{ }
 };
