ee1c9b69c4e9dd88bcb2b4a335ddfbd3657551ca
Chen-Yu Tsai <wenst@chromium.org>
2026-07-24 16:43:17 +0800

    usb: hub: Pass |struct usb_port*| to usb_port_is_power_on()
    usb_port_is_power_on() currently takes |struct usb_hub*|, but only needs
    it to tell if the hub/port is SuperSpeed or not.
    
    In a subsequent change, usb_port_is_power_on() needs access to a pwrseq
    state tracking field in |struct usb_port|. Either structure can be used
    to identify whether a port/hub is SuperSpeed or not, as the field in
    |struct usb_port| is inherited from the hub:
    
        port->is_superspeed = hub_is_superspeed(hub)
    
    Replace usb_port_is_power_on()'s |struct usb_hub*| parameter with
    |struct usb_port*| so a subsequent change can use it.
    
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3281,7 +3281,7 @@ static int check_port_resume_type(struct usb_device *udev,
 	}
 	/* Is the device still present? */
 	else if (status || port_is_suspended(hub, portstatus) ||
-			!usb_port_is_power_on(hub, portstatus)) {
+			!usb_port_is_power_on(port_dev, portstatus)) {
 		if (status >= 0)
 			status = -ENODEV;
 	} else if (!(portstatus & USB_PORT_STAT_CONNECTION)) {
@@ -3723,12 +3723,13 @@ static int wait_for_connected(struct usb_device *udev,
 		struct usb_hub *hub, int port1,
 		u16 *portchange, u16 *portstatus)
 {
+	struct usb_port *port_dev = hub->ports[port1 - 1];
 	int status = 0, delay_ms = 0;
 
 	while (delay_ms < 2000) {
 		if (status || *portstatus & USB_PORT_STAT_CONNECTION)
 			break;
-		if (!usb_port_is_power_on(hub, *portstatus)) {
+		if (!usb_port_is_power_on(port_dev, *portstatus)) {
 			status = -ENODEV;
 			break;
 		}
@@ -5424,7 +5425,7 @@ static void hub_port_connect(struct usb_hub *hub, int port1, u16 portstatus,
 		 * but only if the port isn't owned by someone else.
 		 */
 		if (hub_is_port_power_switchable(hub)
-				&& !usb_port_is_power_on(hub, portstatus)
+				&& !usb_port_is_power_on(port_dev, portstatus)
 				&& !port_dev->port_owner)
 			set_port_feature(hdev, port1, USB_PORT_FEAT_POWER);
 
--- drivers/usb/core/port.h
+++ drivers/usb/core/port.h
@@ -63,6 +63,4 @@ struct usb_port {
 #define to_usb_port(_dev) \
 	container_of(_dev, struct usb_port, dev)
 
-struct usb_hub;
-
-int usb_port_is_power_on(struct usb_hub *hub, unsigned int portstatus);
+int usb_port_is_power_on(struct usb_port *port, unsigned int portstatus);
