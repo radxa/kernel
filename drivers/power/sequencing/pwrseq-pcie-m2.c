7e570b79b224a709e733781b5acbc2db20095d39
Chen-Yu Tsai <wenst@chromium.org>
2026-07-24 16:43:21 +0800

    power: sequencing: pcie-m2: support matching on remote "port" node
    A USB hub can have multiple ports, and this driver needs to
    differentiate which port is being matched to. The USB hub driver now
    associates the "port" node with the usb_port device, so here we can
    use the remote "port" node to check for a match. Then fall back to
    the remote device node for the other connection types.
    
    Also rewrite the existing "remote == dev_of_node(dev)" with
    device_match_of_node() for consistency.
    
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>

--- drivers/power/sequencing/pwrseq-pcie-m2.c
+++ drivers/power/sequencing/pwrseq-pcie-m2.c
@@ -168,9 +168,16 @@ static int pwrseq_pcie_m2_match(struct pwrseq_device *pwrseq,
 	 * parent matches the OF node of 'dev'.
 	 */
 	for_each_endpoint_of_node(ctx->of_node, endpoint) {
+		/* USB port devices are tied to the port nodes. */
+		struct device_node *remote_port __free(device_node) =
+				of_graph_get_remote_port(endpoint);
+		if (device_match_of_node(dev, remote_port))
+			return PWRSEQ_MATCH_OK;
+
+		/* Try the remote port parent for other types. */
 		struct device_node *remote __free(device_node) =
 				of_graph_get_remote_port_parent(endpoint);
-		if (remote && (remote == dev_of_node(dev)))
+		if (device_match_of_node(dev, remote))
 			return PWRSEQ_MATCH_OK;
 	}
 
